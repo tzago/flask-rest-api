ba5030ba4dd409c0da1f87c39b7f468dc400d96d
Thiago Zago <thiago.zago@intersec.com>
2019-02-24 19:09:23 -0500

    Postgres addon implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,7 +12,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') #if env not found use 2nd param as default
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "tzago" # this line is necessary from the Token to be created if removed token doesn't work
 api = Api(app)
